25976b19391620000012920e7be1e6780a010d2e
sfpgmr <sfpgmr@example.com>
2018-10-02 21:28:31 +0900

    update

--- wwwnode/test.js
+++
@@ -1,6 +0,0 @@
-var page = require('webpage').create();
-page.open("https://twitter.com/SFPGMR/", function (status) {
-    console.log(status);
-    page.render("render.png");
-    phantom.exit();
-});

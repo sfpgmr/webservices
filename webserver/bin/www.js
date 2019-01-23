8b938f10ff21ec10557c967a25ba8272136e8bed
sfpgmr <sfpgmr@example.com>
2019-01-24 05:31:29 +0900

    update

--- webserver/bin/www.js
+++ webserver/bin/www.js
@@ -1588,6 +1588,8 @@ function handler(req,res){
       , event = req.headers['x-github-event']
       , id    = req.headers['x-github-delivery'];
 
+      console.log('** sig **:',sig,event,id);
+
   
     if(event == 'push' && payload.repository.name === 'blog'){
       console.log('プッシュイベントを受信:%s to %s',
@@ -1667,8 +1669,9 @@ app.use(logger('combined'));
 
 app.use(xhub({ algorithm: 'sha1', secret: fs.readFileSync(resolveHome('~/www/node/keys/webhook/secret'),'utf-8').trim() }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({ extended: false,limit:'50mb' }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {

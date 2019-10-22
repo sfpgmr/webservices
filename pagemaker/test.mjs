eb8c34ef877431cd4a9fdfcaa9544c06a54ebb45
sfpmr <sfpg@sfpg.com>
2019-10-23 06:20:09 +0900

    update

---
+++ pagemaker/test.mjs
@@ -0,0 +1,19 @@
+import sqlite3 from 'sqlite3';
+
+const db = new sqlite3.Database('./test.db');
+db.serialize(function() {
+  db.run("PRAGMA KEY='test'");
+  db.run("CREATE TABLE lorem (info TEXT)");
+
+  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+  for (var i = 0; i < 10; i++) {
+      stmt.run("Ipsum " + i);
+  }
+  stmt.finalize();
+
+  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      console.log(row.id + ": " + row.info);
+  });
+});
+
+db.close();

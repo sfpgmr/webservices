aa3a1b673427d72f5048a9c42c1a8c9e97fe8101
sfpmr <sfpg@sfpg.com>
2019-12-26 06:01:34 +0900

    upadate

--- webserver/app.mjs
+++ webserver/app.mjs
@@ -61,19 +61,19 @@ app.use(mount('/',serve(resolveHome('~/www/html/contents/'),serveOpts)));
 
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
-// app.use(async (ctx, next) =>{
-//   // set locals, only providing error in development
-//   ctx.response.res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(async (ctx, next) =>{
+  // set locals, only providing error in development
+  ctx.response.res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('./error',{message:'error',error:err});
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('./error',{message:'error',error:err});
+});
 
 export default app;
